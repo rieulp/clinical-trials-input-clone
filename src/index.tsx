54bd7cc0d6c5ce328a314d8561fb434bc590c2c9
rayoungpark <rayoung1229@gmail.com>
2022-11-10 13:01:50 +0900

    feat: 질병 목록 불러오기 기능 추가
    - recoil selectorFamily에서 데이터 통신으로 키워드가 포함된 질병 목록을 받아온다.
    - recoil에 전역 상태로 키워드별로 질병 목록을 캐시 처리했다.

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <RecoilRoot>
+          <GlobalStyle />
+          <App />
+        </RecoilRoot>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
